affd94fbf48f76e4d6aac2a634f4d35f8f6cb726
HS <cris_lx@naver.com>
2023-04-10 19:15:49 +0900

    어노테이션 만들기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,16 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy") // 이름 붙여서 주입
+//@Primary // 우선순위를 1순위로
+@MainDiscountPolicy // 내가 만든 어노테이션
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
