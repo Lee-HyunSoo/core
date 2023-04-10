9fd19fadb0c8d3cef2b24f89c631d5604207e00c
HS <cris_lx@naver.com>
2023-04-10 16:57:56 +0900

    filter

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        // 탐색 시작 위치 지정, 다 스캔하면 오래걸리니까 시간단축가능
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
         // 컴포넌트스캔할 때 안할거 지정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
