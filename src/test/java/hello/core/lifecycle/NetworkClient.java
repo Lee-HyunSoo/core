0c6fcb3062b38bd4d7f35de56dd2bbbddc0bf952
HS <cris_lx@naver.com>
2023-04-12 14:55:55 +0900

    어노테이션으로 빈 생명주기 콜백

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,12 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+//import org.springframework.beans.factory.DisposableBean;
+//import org.springframework.beans.factory.InitializingBean;
+//implements InitializingBean, DisposableBean
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -28,16 +31,28 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    // 프로퍼티들이 세팅이 끝나면 (의존관계 주입이 끝나면) 호출해주겠다
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
-    // 빈이 종료될때 호출
-    @Override
-    public void destroy() throws Exception {
+
+    @PreDestroy
+    public void close() {
         disconnect();
     }
+
+    // 프로퍼티들이 세팅이 끝나면 (의존관계 주입이 끝나면) 호출해주겠다
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메세지");
+//    }
+    // 빈이 종료될때 호출
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
 }
